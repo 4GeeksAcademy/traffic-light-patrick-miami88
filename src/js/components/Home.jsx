c15b63e6c55a98ce4b4a7b5f4c4e2afaf5dd8f9b
Patrick <patcthomas28@gmail.com>
2026-02-13 23:14:52 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const colors = ["red", "yellow", "green"];
+	const [current, setCurrent] = useState(colors[0]);
+	const [currentIndex, setCurrentIndex] = useState(0)
+	
+	const setLightColors = (index) => {
+		setCurrentIndex(index);
+		setCurrent(colors[index])
+		return
+	}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+		<div className="text-center d-flex flex-column align-items-center">
+			<div className="d-flex justify-content-center h1 p-3">Hey look, it's a stop light</div>
+            <div className="stop-light-top mt-4"></div>
+			<div className="stop-light-box d-flex flex-column align-items-center">
+				<div className={"stop-lights red-light " + (current === "red" ? "active-red-light" : "")} onClick={() => setLightColors(0)}></div>
+				<div className={"stop-lights yellow-light " + (current === "yellow" ? "active-yellow-light" : "")} onClick={() => setLightColors(1)}></div>
+				<div className={"stop-lights green-light " + (current === "green" ? "active-green-light" : "")} onClick={() => setLightColors(2)}></div>
+			</div>
 		</div>
 	);
 };
